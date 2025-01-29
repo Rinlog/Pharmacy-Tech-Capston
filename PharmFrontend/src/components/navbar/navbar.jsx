c675f4678385a6b2f9ff4ac3752c56947a6c63df
Rinlog <Sadikulz123@gmail.com>
2025-01-29 09:50:27 -0400

    fixed logout button not removing cookies

--- PharmFrontend/src/components/navbar/navbar.jsx
+++ PharmFrontend/src/components/navbar/navbar.jsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
-import { Cookies } from "react-cookie";
+import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
 import { CheckAuth } from "@components/login/CheckAuth";
 import "./Navbar.css";
@@ -7,12 +7,16 @@ import "./Navbar.css";
 function Navbar() {
   const authState = CheckAuth();
   const navigate = useNavigate();
-  const cookies = new Cookies();
+  const [cookies, setCookie] = useCookies(['user', 'admin']);
   const location = useLocation(); // Get the current route
 
   const Logout = () => {
-    cookies.remove("user");
-    cookies.remove("admin");
+    let currentDate = new Date()
+    let pastDate = currentDate - new Date(100000)
+    let pastStringDate = new Date(pastDate) 
+
+    setCookie("user",null,{expires: pastStringDate})
+    setCookie("admin",null,{expires: pastStringDate})
     navigate("/login");
     //window.location.reload(); //commented this out testing to see if i need it still
   };
