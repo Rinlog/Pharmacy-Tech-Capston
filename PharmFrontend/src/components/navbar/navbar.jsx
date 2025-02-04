130bc603c54448cae5e0adb1a6f2f9dbff93fbfe
Rinlog <Sadikulz123@gmail.com>
2025-02-04 13:30:05 -0400

    fixed some issues with centered title

--- PharmFrontend/src/components/navbar/navbar.jsx
+++ PharmFrontend/src/components/navbar/navbar.jsx
@@ -19,7 +19,57 @@ function Navbar() {
 
   // Check if the current path is the home page
   const isHomePage = location.pathname === "/home";
+  const NavContent = (<div className="flex-fill">
+        {!isHomePage && authState.loggedIn && (
+        <>
+        <Link to="/home" className="navbar-item">
+            Home
+          </Link>
+          <Link to="/drugs" className="navbar-item">
+            Drugs
+          </Link>
+          <Link to="/physicians" className="navbar-item">
+            Physicians
+          </Link>
+          <Link to="/patients" className="navbar-item">
+            Patients
+          </Link>
+          <Link to="/orders" className="navbar-item">
+            Orders
+          </Link>
+          <Link to="/verification" className="navbar-item">
+            Verification
+          </Link>
 
+          {authState.loggedIn && authState.isAdmin && (
+            <>
+              <Link to="/usermanagement" className="navbar-item">
+                User Management
+              </Link>
+              <Link to="/logs" className="navbar-item">
+                Logs
+              </Link>
+            </>
+          )}
+          <button className="navbar-button" onClick={Logout}>
+          Logout
+        </button>
+        </>
+      )}
+      {authState.loggedIn && isHomePage && (
+        <div className="d-flex align-items-center justify-content-right flex-fill">
+          <div className="d-flex navbar-hometext justify-content-center flex-fill">
+            Welcome to the NBCC Pharmaceutical Tech System!
+          </div>
+          <div className="logout-container align-self-right pl-5">
+            <button className="navbar-button" onClick={Logout}>
+                Logout
+            </button>
+          </div>
+        </div>
+      )}
+  </div>)
+  
   return (
     <nav className="navbar">
       <div className="navbar-brand">
@@ -34,56 +84,11 @@ function Navbar() {
       </div>
 
       <div className="navbar-menu" id="navbarMenu">
-        <div className="navbar-links">
-          {!isHomePage && authState.loggedIn && (
-            <>
-            <Link to="/home" className="navbar-item">
-                Home
-              </Link>
-              <Link to="/drugs" className="navbar-item">
-                Drugs
-              </Link>
-              <Link to="/physicians" className="navbar-item">
-                Physicians
-              </Link>
-              <Link to="/patients" className="navbar-item">
-                Patients
-              </Link>
-              <Link to="/orders" className="navbar-item">
-                Orders
-              </Link>
-              <Link to="/verification" className="navbar-item">
-                Verification
-              </Link>
-
-              {authState.loggedIn && authState.isAdmin && (
-                <>
-                  <Link to="/usermanagement" className="navbar-item">
-                    User Management
-                  </Link>
-                  <Link to="/logs" className="navbar-item">
-                    Logs
-                  </Link>
-                </>
-              )}
-              <button className="navbar-button" onClick={Logout}>
-              Logout
-            </button>
-            </>
-          )}
-          {authState.loggedIn && isHomePage && (
-            <div className="d-flex align-items-center justify-content-right flex-fill">
-              <div className="d-flex navbar-hometext justify-content-center flex-fill">
-                Welcome to the NBCC Pharmaceutical Tech System!
-              </div>
-              <div className="logout-container align-self-right pl-5">
-                <button className="navbar-button" onClick={Logout}>
-                    Logout
-                </button>
-              </div>
-            </div>
-          )}
-        </div>
+      {authState.loggedIn && isHomePage && (<div className="navbar-links flex-fill">
+        {NavContent}
+      </div>) || authState.loggedIn && !isHomePage && (<div className="navbar-links">
+        {NavContent}
+      </div>)}
       </div>
     </nav>
   );
