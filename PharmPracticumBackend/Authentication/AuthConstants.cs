751007bbe030bd2d179dc0a09fd03331141f3535
Rinlog <Sadikulz123@gmail.com>
2025-02-06 14:08:37 -0400

    testing if appsettings still is part of git, it should be

---
+++ PharmPracticumBackend/Authentication/AuthConstants.cs
@@ -0,0 +1,15 @@
+﻿namespace PharmPracticumBackend.Authentication
+{
+    public  class AuthConstants
+    {
+        private readonly string key;
+
+        public AuthConstants(IConfiguration configuration)
+        {
+            key = configuration.GetValue<String>("ExpectedHash:Hash");
+        }
+        
+
+
+    }
+}
