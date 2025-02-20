5d013489cae3ead36e760948a41bf01abedfffc1
Sneakyweasel90 <134094143+Sneakyweasel90@users.noreply.github.com>
2025-02-19 21:24:19 -0400

    Sort by now works with numbers and letters combined

--- PharmFrontend/src/components/headerSort/sortByHeader.jsx
+++ PharmFrontend/src/components/headerSort/sortByHeader.jsx
@@ -4,10 +4,25 @@ function SortbyHeader(filteredData, header,direction){
         let first = a[header];
         let second = b[header];
 
-        //convert to lowercase strings if there is upcase letters
-        first = String(first).toLowerCase();
-        second = String(second).toLowerCase();
- 
+        //need this here to pull the number from the text
+        const extractNumber = (value) => {
+            if (typeof value === "number") {
+                return value;
+            }
+            
+            //need to convert the string to a number
+            const num = parseFloat(value);
+            
+            if (isNaN(num)) {
+                return value.toString().toLowerCase();
+            } 
+            else {
+                return num;
+            }
+        };
+
+        first = extractNumber(first);
+        second = extractNumber(second);
 
         //sort info for asc and dsc rows
         if (direction === 'asc') {
