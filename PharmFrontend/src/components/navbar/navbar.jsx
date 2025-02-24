8d58443ae9d7f5f85439a8e07773ac297aa7e619
Rinlog <Sadikulz123@gmail.com>
2025-02-24 16:16:42 -0400

    modifed user icon a little bit

--- PharmFrontend/src/components/navbar/navbar.jsx
+++ PharmFrontend/src/components/navbar/navbar.jsx
@@ -94,7 +94,7 @@ function Navbar() {
           <Dropdown className="pl-2">
             <Dropdown.Toggle className='HideButtonCSS'>
               <span>
-                <svg height="30px" width="30px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 60.671 60.671" xmlSpace="preserve" fill="#000000">
+                <svg height="30px" width="30px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 60.671 60.671" xmlSpace="preserve" fill="#000000" className="UserIcon">
                   <g id="SVGRepo_bgCarrier" strokeWidth="0"/>
                   <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"/>
                   <g id="SVGRepo_iconCarrier"> <g> <g> <ellipse style={{fill:"#010002"}} cx="30.336" cy="12.097" rx="11.997" ry="12.097"/> <path style={{fill:"#010002"}} d="M35.64,30.079H25.031c-7.021,0-12.714,5.739-12.714,12.821v17.771h36.037V42.9 C48.354,35.818,42.661,30.079,35.64,30.079z"/> </g> </g> </g>
@@ -119,7 +119,7 @@ function Navbar() {
             <Dropdown>
               <Dropdown.Toggle className='HideButtonCSS'>
                 <span>
-                  <svg height="30px" width="30px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 60.671 60.671" xmlSpace="preserve" fill="#000000">
+                  <svg height="30px" width="30px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 60.671 60.671" xmlSpace="preserve" fill="#000000" className="UserIcon">
                     <g id="SVGRepo_bgCarrier" strokeWidth="0"/>
                     <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"/>
                     <g id="SVGRepo_iconCarrier"> <g> <g> <ellipse style={{fill:"#010002"}} cx="30.336" cy="12.097" rx="11.997" ry="12.097"/> <path style={{fill:"#010002"}} d="M35.64,30.079H25.031c-7.021,0-12.714,5.739-12.714,12.821v17.771h36.037V42.9 C48.354,35.818,42.661,30.079,35.64,30.079z"/> </g> </g> </g>
