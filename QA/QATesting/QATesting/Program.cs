24355b1b21acf871b51b92605033612a9bbfcdab
Rinlog <Sadikulz123@gmail.com>
2025-03-13 12:03:23 -0300

    added a base layout for how our qa testing environment

---
+++ QA/QATesting/QATesting/Program.cs
@@ -0,0 +1,50 @@
+﻿//this will be the main zone where we execute all our tests
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using QATesting.SiteTests;
+
+string Path = System.IO.Path.GetFullPath("../../../../../GeckoDriver");
+Console.WriteLine("Path to GeckoDriver: "+ Path);
+string BaseUrl = "https://pharmtech.nbcc.ca"; //feel free to change this to a local frontend ip.
+Console.WriteLine("Site Testing: "+ BaseUrl);
+
+//tests executing
+IWebDriver driver = new FirefoxDriver(Path);
+driver.Url = BaseUrl;
+bool US3ValidLogin = LoginTests.TestValidLogin(driver,BaseUrl);
+driver.Quit();
+
+
+
+
+
+
+//Setup for outputting results nicely
+Console.Clear();
+Console.WriteLine("################## RESULTS ##################");
+Console.WriteLine();
+
+//output all results below
+
+OutPutResult(US3ValidLogin, "US3ValidLogin: ");
+
+
+Console.ReadLine();
+
+
+//used to display nice result messages for people running the tests
+void OutPutResult(bool result, string PreMessage)
+{
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.Write(PreMessage);
+    if (result == true)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("PASS");
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("FAIL");
+    }
+}
---
+++ QA/QATesting/QATesting/SitePageElements/LoginElements.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace QATesting.SitePageElements
+{
+    internal class LoginElements
+    {
+        public static IWebElement LoginEmail(IWebDriver driver) {
+            return driver.FindElement(By.CssSelector("input[qa-id='Email']"));
+        }
+        public static IWebElement LoginPass(IWebDriver driver)
+        {
+            return driver.FindElement(By.CssSelector("input[qa-id='Password']"));
+        }
+        public static IWebElement LoginSubmitButton(IWebDriver driver)
+        {
+            return driver.FindElement(By.CssSelector("button[qa-id='Login']"));
+        }
+        public static IWebElement LoginSignUp(IWebDriver driver)
+        {
+            return driver.FindElement(By.CssSelector("button[qa-id='SignUp']"));
+        }
+        public static IWebElement LoginResetPass(IWebDriver driver)
+        {
+            return driver.FindElement(By.CssSelector("button[qa-id='ResetPass']"));
+        }
+    }
+}
