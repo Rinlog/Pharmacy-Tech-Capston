72e3c806d13741a67679164f680ee3de4039d123
Sneakyweasel90 <134094143+Sneakyweasel90@users.noreply.github.com>
2025-03-13 16:05:11 -0300

    Button pushing tests added
    Testing these is not needed but need them for later

--- QA/QATesting/QATesting/Program.cs
+++ QA/QATesting/QATesting/Program.cs
@@ -11,7 +11,7 @@ Console.WriteLine("Site Testing: "+ BaseUrl);
 //Valid Login Test
 IWebDriver driver = new FirefoxDriver(Path);
 driver.Url = BaseUrl;
-bool US3ValidLogin = LoginTests.TestValidLogin(driver,BaseUrl);
+bool US3ValidLogin = LoginTests.TestValidLogin(driver, BaseUrl);
 Console.Clear();
 driver.Quit();
 
@@ -21,6 +21,41 @@ driver1.Url = BaseUrl;
 bool US3FailedLogin = LoginTests.TestFailedLogin(driver1, BaseUrl);
 Console.Clear();
 driver1.Quit();
+
+//Drug Button Test
+IWebDriver driver2 = new FirefoxDriver(Path);
+driver2.Url = BaseUrl;
+bool US3DrugButton = HomePageButtons.TestDrugsButton(driver2, BaseUrl);
+Console.Clear();
+driver2.Quit();
+
+//Physician Button Test
+IWebDriver driver3 = new FirefoxDriver(Path);
+driver3.Url = BaseUrl;
+bool US3PhysicianButton = HomePageButtons.TestPhysiciansButton(driver3, BaseUrl);
+Console.Clear();
+driver3.Quit();
+
+//Patients Button Test
+IWebDriver driver4 = new FirefoxDriver(Path);
+driver4.Url = BaseUrl;
+bool US3PatientsButton = HomePageButtons.TestPatientsButton(driver4, BaseUrl);
+Console.Clear();
+driver4.Quit();
+
+//Orders Button Test
+IWebDriver driver5 = new FirefoxDriver(Path);
+driver5.Url = BaseUrl;
+bool US4OrdersButton = HomePageButtons.TestOrdersButton(driver5, BaseUrl);
+Console.Clear();
+driver5.Quit();
+
+//Verification Button Test
+IWebDriver driver6 = new FirefoxDriver(Path);
+driver6.Url = BaseUrl;
+bool US4VerificationButton = HomePageButtons.TestVerificationButton(driver6, BaseUrl);
+Console.Clear();
+driver6.Quit();
 
 
 //Setup for outputting results nicely
@@ -32,6 +67,11 @@ Console.WriteLine();
 
 OutPutResult(US3ValidLogin, "US3ValidLogin: ");
 OutPutResult(US3FailedLogin, "US3FailedLogin: ");
+OutPutResult(US3DrugButton, "US3DrugButton: ");
+OutPutResult(US3PhysicianButton, "US3PhysicianButton: ");
+OutPutResult(US3PatientsButton, "US3PatientsButton: ");
+OutPutResult(US4OrdersButton, "US4OrdersButton: ");
+OutPutResult(US4VerificationButton, "US4VerificationButton: ");
 
 
 Console.ReadLine();
---
+++ QA/QATesting/QATesting/SitePageElements/HomeElements.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace QATesting.SitePageElements
+{
+    internal class HomeElements
+    {
+        public static IWebElement DrugsButton(IWebDriver driver)
+        {
+            return driver.FindElement(By.CssSelector("html body div#root div.page-container div.home-container div.home-content div.button-grid div.button-with-image a.home-button"));
+        }
+
+        public static IWebElement PhysicianButton(IWebDriver driver)
+        {
+            return driver.FindElement(By.CssSelector("div.button-with-image:nth-child(2) > a:nth-child(1)"));
+        }
+
+        public static IWebElement PatientsButton(IWebDriver driver)
+        {
+            return driver.FindElement(By.CssSelector("div.button-with-image:nth-child(3) > a:nth-child(2)"));
+        }
+
+        public static IWebElement OrdersButton(IWebDriver driver)
+        {
+            return driver.FindElement(By.CssSelector("div.button-with-image:nth-child(4) > a:nth-child(1)"));
+        }
+
+        public static IWebElement VerificationButton(IWebDriver driver)
+        {
+            return driver.FindElement(By.CssSelector("div.button-with-image:nth-child(5) > a:nth-child(2)"));
+        }
+
+        public static IWebElement UserManagementButton(IWebDriver driver)
+        {
+            return driver.FindElement(By.XPath("/html/body/div/div/div[2]/div[2]/div/div[7]/a"));
+        }
+
+        public static IWebElement LogoutButton(IWebDriver driver)
+        {
+            return driver.FindElement(By.XPath("/html/body/div/div/div[2]/div[2]/div/div[8]/a"));
+        }
+    }
+}
