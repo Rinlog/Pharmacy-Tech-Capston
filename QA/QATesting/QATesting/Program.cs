21b0cbc4d7b09c2ccc0e11e4fde2fa62473c065d
Sneakyweasel90 <134094143+Sneakyweasel90@users.noreply.github.com>
2025-03-13 14:19:38 -0300

    Failed login added

--- QA/QATesting/QATesting/Program.cs
+++ QA/QATesting/QATesting/Program.cs
@@ -8,15 +8,19 @@ Console.WriteLine("Path to GeckoDriver: "+ Path);
 string BaseUrl = "https://pharmtech.nbcc.ca"; //feel free to change this to a local frontend ip.
 Console.WriteLine("Site Testing: "+ BaseUrl);
 
-//tests executing
+//Valid Login Test
 IWebDriver driver = new FirefoxDriver(Path);
 driver.Url = BaseUrl;
 bool US3ValidLogin = LoginTests.TestValidLogin(driver,BaseUrl);
+Console.Clear();
 driver.Quit();
 
-
-
-
+//Failed Login Test
+IWebDriver driver1 = new FirefoxDriver(Path);
+driver1.Url = BaseUrl;
+bool US3FailedLogin = LoginTests.TestFailedLogin(driver1, BaseUrl);
+Console.Clear();
+driver1.Quit();
 
 
 //Setup for outputting results nicely
@@ -27,6 +31,7 @@ Console.WriteLine();
 //output all results below
 
 OutPutResult(US3ValidLogin, "US3ValidLogin: ");
+OutPutResult(US3FailedLogin, "US3FailedLogin: ");
 
 
 Console.ReadLine();
--- QA/QATesting/QATesting/SiteTests/LoginTests.cs
+++ QA/QATesting/QATesting/SiteTests/LoginTests.cs
@@ -23,12 +23,40 @@ namespace QATesting.SiteTests
             Pass.SendKeys("Password1!");
             Thread.Sleep(1000);
             Submit.Click();
+            Thread.Sleep(1000);
 
             if (driver.Url == BaseUrl+"/login")
             {
                 return false;
             }
             else if (driver.Url == BaseUrl+"/home")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool TestFailedLogin(IWebDriver driver, string BaseUrl)
+        {
+            IWebElement Email = LoginElements.LoginEmail(driver);
+            IWebElement Pass = LoginElements.LoginPass(driver);
+            IWebElement Submit = LoginElements.LoginSubmitButton(driver);
+
+            //account must be valid that we are testing with
+            Email.SendKeys("test@nbcc.ca");
+            Pass.SendKeys("Password!");
+            Thread.Sleep(1000);
+            Submit.Click();
+            Thread.Sleep(1000);
+
+            if (driver.Url == BaseUrl + "/login")
+            {
+                return false;
+            }
+            else if (driver.Url == BaseUrl + "/home")
             {
                 return true;
             }
